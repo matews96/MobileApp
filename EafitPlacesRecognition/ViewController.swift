d53f313123aa4cb59625d7563502995440378215
Mateo Echeverry <Mateo.X.Echeverry.-ND@disney.com>
2018-06-03 15:12:05 -0500

    Initial Commit

---
+++ EafitPlacesRecognition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EafitPlacesRecognition
+//
+//  Created by Mateo Echeverri Yepes on 6/3/18.
+//  Copyright © 2018 Mateo Echeverri Yepes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
